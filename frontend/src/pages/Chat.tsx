6a3f2f49863da6d146b30a385ba93ce95e82f811
Pham Phat <phamphat2003vn@gmail.com>
2024-07-29 23:18:17 +0700

    code Chat UI with Material UI

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,7 +1,205 @@
+import { Box, Avatar, Typography, Button } from "@mui/material";
 import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { colors, IconButton } from "@mui/material";
+import ChatItem from "./../components/chat/ChatItem";
+import { IoMdSend } from "react-icons/io";
+
+const chatMessages = [
+    {
+        role: "assistant",
+        content:
+            "The 2020 World Series was played in Texas at Globe Life Field in Arlington.",
+    },
+    {
+        role: "assistant",
+        content:
+            "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+    },
+    {
+        role: "user",
+        content:
+            "Compose a poem that explains the concept of recursion in programming.",
+    },
+    {
+        role: "assistant",
+        content:
+            "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.",
+    },
+    {
+        role: "user",
+        content:
+            "Compose a poem that explains the concept of recursion in programming.",
+    },
+];
 
 const Chat = () => {
-    return <div>Chat</div>;
+    const auth = useAuth();
+    return (
+        //using box & avatar from material UI mui
+        <Box
+            sx={{
+                display: "flex",
+                flex: 1,
+                alignItems: "center",
+                width: "100%",
+                height: "100%",
+                mt: 5,
+                gap: 3,
+            }}
+        >
+            <Box
+                sx={{
+                    display: { md: "flex", xs: "none", sm: "none" },
+                    flex: 0.2,
+                    flexDirection: "column",
+                }}
+            >
+                <Box
+                    sx={{
+                        display: "flex",
+                        width: "100%",
+                        height: "60vh",
+                        bgcolor: "rgb(17,29,39)",
+                        borderRadius: 4,
+                        boxShadow: "0px 0px 10px #000",
+                        flexDirection: "column",
+                        mx: 3,
+                        mt: 4,
+                        py: 3,
+                    }}
+                >
+                    <Avatar
+                        sx={{
+                            mx: "auto",
+                            my: 2,
+                            bgcolor: "white",
+                            color: "black",
+                            fontWeight: 700,
+                        }}
+                    >
+                        {auth?.user?.name[0]}
+                        {auth?.user?.name.split(" ")[1][0]}
+                    </Avatar>
+                    <Typography sx={{ mx: "auto", fontFamily: "work sans" }}>
+                        You are talking to a Chatbot
+                    </Typography>
+                    <Typography
+                        sx={{
+                            mx: "auto",
+                            fontFamily: "work sans",
+                            my: 4,
+                            p: 3,
+                        }}
+                    >
+                        You can ask some questions related to Knowledge,
+                        Business, Economices, Education,etc. But avoid sharing
+                        personal information, sensitive policities
+                    </Typography>
+                    <Button
+                        sx={{
+                            width: "200px",
+                            my: "auto",
+                            color: "white",
+                            fontFamily: "700",
+                            borderRadius: 4,
+                            py: 1.8,
+                            mx: "auto",
+
+                            bgcolor: colors.red[400],
+                            transition: "all .4s",
+                            ":hover": {
+                                bgcolor: colors.red.A700,
+                                filter: "drop-shadow(5px 5px 20px rgb(121, 4, 255))",
+                            },
+                        }}
+                    >
+                        Clear Conversation
+                    </Button>
+                </Box>
+            </Box>
+            <Box
+                sx={{
+                    display: "flex",
+                    flex: { md: 0.8, xs: 1, sm: 1 },
+                    flexDirection: "column",
+                    px: 3,
+                }}
+            >
+                <Typography
+                    sx={{
+                        mx: "auto",
+                        display: "flex",
+                        alignItems: "center",
+                        fontSize: "40px",
+                        color: "white",
+                        mb: 2,
+                    }}
+                >
+                    GPT-3.5 Turbo
+                    <img
+                        src="openai.png"
+                        alt="openai"
+                        style={{ marginLeft: "10px" }}
+                        width={"40px"}
+                        height={"40px"}
+                        className="image-main"
+                    />
+                </Typography>
+                <Box
+                    sx={{
+                        width: "100%",
+                        height: "60vh",
+                        borderRadius: "3",
+                        mx: "auto",
+                        display: "flex",
+                        flexDirection: "column",
+                        overflow: "scroll",
+                        overflowX: "hidden",
+                        overflowY: "auto",
+                        scrollBehavior: "smooth",
+                    }}
+                >
+                    {chatMessages.map((chat, index) => (
+                        <ChatItem
+                            content={chat.content}
+                            role={chat.role}
+                            key={index}
+                        />
+                    ))}
+                </Box>
+                <div
+                    style={{
+                        width: "100%",
+                        padding: "20px",
+                        borderRadius: 8,
+                        backgroundColor: "rgb(17,27,39)",
+                        display: "flex",
+                    }}
+                >
+                    <input
+                        type="text"
+                        style={{
+                            width: "100%",
+                            textWrap: "wrap",
+                            backgroundColor: "transparent",
+                            padding: "10px",
+                            border: "none",
+                            outline: "none",
+                            color: "white",
+                            fontSize: "20px",
+                        }}
+                    />
+                    <IconButton
+                        sx={{ ml: "auto", color: "white", fontSize: 30, pr: 5 }}
+                    >
+                        {" "}
+                        <IoMdSend></IoMdSend>
+                    </IconButton>
+                </div>
+            </Box>
+        </Box>
+    );
 };
 
 export default Chat;
