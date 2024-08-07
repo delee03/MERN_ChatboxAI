0e5cf23abfd0678196b761feaf0767f07a38b950
Pham Phat <phamphat2003vn@gmail.com>
2024-08-07 23:48:34 +0700

    chore: Update npm dependencies for react-router-dom and react-syntax-highlighter

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -1,6 +1,36 @@
 import React from "react";
 import { Avatar, Box, Typography } from "@mui/material";
 import { useAuth } from "../../context/AuthContext";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import {
+    coldarkCold,
+    coldarkDark,
+} from "react-syntax-highlighter/dist/esm/styles/prism";
+
+// hàm trích xuất code từ chuỗi
+function extractCodeFromString(message: string) {
+    if (message.includes("```")) {
+        const codeBlocks = message.split("```");
+        return codeBlocks;
+    }
+}
+// kiểm tra có phải là code không
+function iscodeBlock(message: string) {
+    if (
+        message.includes("=") ||
+        message.includes(";") ||
+        message.includes("{") ||
+        message.includes("}") ||
+        message.includes("(") ||
+        message.includes(")") ||
+        message.includes("#") ||
+        message.includes("]") ||
+        message.includes("//")
+    ) {
+        return true;
+    }
+    return false;
+}
 
 const ChatItem = ({
     content,
@@ -9,6 +39,7 @@ const ChatItem = ({
     content: string;
     role: "user" | "assistant";
 }) => {
+    const messageBlocks = extractCodeFromString(content);
     const auth = useAuth();
     return role === "assistant" ? (
         <Box sx={{ display: "flex", p: 2, bgcolor: "#1A1A1C", my: 2, gap: 2 }}>
@@ -16,18 +47,57 @@ const ChatItem = ({
                 <img src="openai.png" width={30} alt="openai" />
             </Avatar>
             <Box>
-                <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+                {messageBlocks ? (
+                    messageBlocks.map((block, index) => {
+                        return iscodeBlock(block) ? (
+                            <SyntaxHighlighter
+                                key={index}
+                                language="javascript"
+                                style={coldarkDark}
+                            >
+                                {block}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <Typography key={index} sx={{ fontSize: "20px" }}>
+                                {block}
+                            </Typography>
+                        );
+                    })
+                ) : (
+                    <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+                )}
             </Box>
         </Box>
     ) : (
-        <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", gap: 2 }}>
+        <Box sx={{ display: "flex", p: 2, bgcolor: "#004d56", gap: 2, my: 2 }}>
             <Avatar sx={{ ml: 0, bgcolor: "black", color: "white" }}>
                 {auth?.user?.name[0]}
                 {auth?.user?.name.split(" ")[1][0]}
             </Avatar>
             <Box>
-                <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+                {messageBlocks ? (
+                    messageBlocks.map((block, index) => {
+                        return iscodeBlock(block) ? (
+                            <SyntaxHighlighter
+                                key={index}
+                                language="javascript"
+                                style={coldarkDark}
+                            >
+                                {block}
+                            </SyntaxHighlighter>
+                        ) : (
+                            <Typography key={index} sx={{ fontSize: "20px" }}>
+                                {block}
+                            </Typography>
+                        );
+                    })
+                ) : (
+                    <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+                )}
             </Box>
+            {/* <Box>
+                <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+            </Box> */}
         </Box>
     );
 };
