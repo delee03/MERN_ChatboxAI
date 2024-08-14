1f931efe4614d32f065d1bf313d221c7a3cbbd49
Pham Phat <phamphat2003vn@gmail.com>
2024-08-14 23:26:07 +0700

    chore: Update npm dependencies and add TypeScript types for Node.js

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,10 +8,14 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL =
-    "http://localhost:5000/api/v1" ||
-    "https://mern-chatbot-api.vercel.app/api/v1";
+const baseURL =
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:5000/api/v1"
+        : "https://mern-chatbot-api.vercel.app/api/v1";
+
+axios.defaults.baseURL = baseURL;
 axios.defaults.withCredentials = true;
+
 //lệnh xác thực cho phép set cookies từ backend trực tiếp, trao đổi thông tin cookies
 
 const theme = createTheme({
