6c37825a2d6eaa0e8c56895ac38123b2f419b554
Pham Phat <phamphat2003vn@gmail.com>
2024-08-14 14:34:40 +0700

    chore: Update cors origin URL in app.ts and set build output directory to "dist" in vite.config.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ config();
 const app = express();
 
 //sử dụng middlewares
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
