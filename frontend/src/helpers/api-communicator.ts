1ec2a8582d2c6c4cea0cbd0f22938ae211a72bd7
Phạm Tiến Thuận Phát <128588793+delee03@users.noreply.github.com>
2024-08-15 15:39:05 +0700

    Update api-communicator.ts

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const baseURL =
-    process.env.NODE_ENV === "development"
-        ? "http://localhost:5000/api/v1"
-        : "https://mern-chatbot-ai-ruddy.vercel.app/api/v1";
+// const baseURL =
+//     process.env.NODE_ENV === "development"
+//         ? "http://localhost:5000/api/v1"
+//         : "https://mern-chatbot-ai-ruddy.vercel.app/api/v1";
 
-axios.defaults.baseURL = baseURL;
+axios.defaults.baseURL = "https://mern-chatbot-ai-ruddy.vercel.app/api/v1";
 axios.defaults.withCredentials = true;
 
 
